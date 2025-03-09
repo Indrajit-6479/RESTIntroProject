b34813c7f98ef8cb698b0cccc852db0dcac348d6
Indrajit Rananavare <indrajit6479@gmail.com>
2025-03-09 14:27:11 +0530

    create method for path variable

--- src/main/java/com/project1/controller/HomeController.java
+++ src/main/java/com/project1/controller/HomeController.java
@@ -1,10 +1,7 @@
 package com.project1.controller;
 
 import com.project1.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
@@ -21,5 +18,10 @@ public class HomeController {
         user.setName("Indrajit");
         user.setEmailId("indrajit6479@gmail.com");
         return user;
+    }
+
+    @GetMapping("/{id}")
+    public String pathVariable(@PathVariable String id) {
+        return String.format("Path variable is: %s", id);
     }
 }
