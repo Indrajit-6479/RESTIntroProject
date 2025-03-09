2f3fbfc0f58764669e3c80c52be0fdf4ba603733
Indrajit Rananavare <indrajit6479@gmail.com>
2025-03-09 14:21:24 +0530

    create user method

--- src/main/java/com/project1/controller/HomeController.java
+++ src/main/java/com/project1/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.project1.controller;
 
+import com.project1.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +11,15 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "Hello World!!!";
+    }
+
+    //@RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("Indrajit");
+        user.setEmailId("indrajit6479@gmail.com");
+        return user;
     }
 }
