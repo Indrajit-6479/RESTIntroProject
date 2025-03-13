f13a31eca210f3c4afcee937e9d76c3a50aceaff
Indrajit Rananavare <indrajit6479@gmail.com>
2025-03-13 09:36:01 +0530

    Added @JsonIgnore for data filtering

--- src/main/java/com/project1/model/Employee.java
+++ src/main/java/com/project1/model/Employee.java
@@ -1,5 +1,6 @@
 package com.project1.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,5 +11,6 @@ public class Employee {
     private String firstName;
     private String lastName;
     private String emailId;
+    @JsonIgnore
     private String department;
 }
