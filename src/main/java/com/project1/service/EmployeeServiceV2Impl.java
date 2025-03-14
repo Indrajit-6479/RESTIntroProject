cec9299b07321ed25c564e6a25a33d233601014d
Indrajit Rananavare <indrajit6479@gmail.com>
2025-03-14 22:25:56 +0530

    *Created API for get all employees data in database *Created API for get employees data by id in database

--- src/main/java/com/project1/service/EmployeeServiceV2Impl.java
+++ src/main/java/com/project1/service/EmployeeServiceV2Impl.java
@@ -1,5 +1,6 @@
 package com.project1.service;
 
+import com.project1.Exceptions.EmployeeNotFoundException;
 import com.project1.entity.EmployeeEntity;
 import com.project1.model.Employee;
 import com.project1.repository.EmployeeRepository;
@@ -8,31 +9,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
-public class EmployeeServiceV2Impl implements EmployeeService{
+public class EmployeeServiceV2Impl implements EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
+
     @Override
     public Employee save(Employee employee) {
-        if (employee.getEmployeeId() == null || employee.getEmployeeId().isEmpty()){
+        if (employee.getEmployeeId() == null || employee.getEmployeeId().isEmpty()) {
             employee.setEmployeeId(UUID.randomUUID().toString());
         }
         EmployeeEntity employeeEntity = new EmployeeEntity();
-        BeanUtils.copyProperties(employee,employeeEntity);
+        BeanUtils.copyProperties(employee, employeeEntity);
         employeeRepository.save(employeeEntity);
         return employee;
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return List.of();
+        List<EmployeeEntity> employeeEntity = employeeRepository.findAll();
+        return employeeEntity.stream()
+                .map(employeeEntity1 -> {
+                    Employee employee = new Employee();
+                    BeanUtils.copyProperties(employeeEntity1, employee);
+                    return employee;
+                }).toList();
     }
 
     @Override
     public Employee getEmployeeById(String id) {
-        return null;
+        Optional<EmployeeEntity> employeeEntity = employeeRepository.findById(id);
+        if (employeeEntity.isPresent()) {
+            Employee employee = new Employee();
+            BeanUtils.copyProperties(employeeEntity.get(), employee);
+            return employee;
+        } else {
+            throw new EmployeeNotFoundException("Employee is not present with id " + id);
+        }
     }
 
     @Override
